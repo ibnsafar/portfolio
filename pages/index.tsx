26bfd7f63d5c6339c2725d05bdd65fb13ccf0822
Yokub1 <yokubibnsafar@gmail.com>
2022-05-30 00:46:08 +0500

    init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+import Header from "../components/Header/Header";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Header/>
+            <h1>Hello from next js</h1>
+        </>
+    )
+}
+
+export default Home
